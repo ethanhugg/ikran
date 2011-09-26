60b61980c296a0cf6ad49a9fd9b9ea997139f845
Enda Mannion <emannion@gmail.com>
2011-09-26 11:07:25 +0100

    returning int from unregister p2p for consistency

--- tests/testapp_softphone/sipcc_controller.h
+++ tests/testapp_softphone/sipcc_controller.h
@@ -91,7 +91,7 @@ public:
 	void EndCall();
 	void AnswerCall();
 	
-	bool StartP2PMode(std::string sipUser);
+	int StartP2PMode(std::string sipUser);
 	void PlaceP2PCall(std::string dial_number,  std::string sipDomain);
 
 	void SetVideoWindow(void* videoWin) {
