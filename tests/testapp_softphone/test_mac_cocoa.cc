1bed6e40ee0d55c7d94d382e5ba393d70584be6f
Ethan Hugg <ehugg@cisco.com>
2011-09-29 19:32:59 -0700

    Prevent access violation if no config file given to test app.

--- tests/testapp_softphone/test_mac_cocoa.cc
+++ tests/testapp_softphone/test_mac_cocoa.cc
@@ -1,13 +1,40 @@
-/*
- *  Copyright (c) 2011 The WebRTC project authors. All Rights Reserved.
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- */
-
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the Cisco Systems SIP Stack.
+ *
+ * The Initial Developer of the Original Code is
+ * Cisco Systems (CSCO).
+ * Portions created by the Initial Developer are Copyright (C) 2002
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Cary Bran <cbran@cisco.com>
+ *  Ethan Hugg <ehugg@cisco.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #include	"test_mac_cocoa.h"
 #include	"test_main.h"
@@ -67,12 +94,11 @@ int main(int argc, const char * argv[]){
 
     [NSApplication sharedApplication];
 	
-	NSString* filePath;
+	NSString* filePath= NULL;
 	if (argc == 2) {
 		//assume that the first arguement is the path to the test file
 		filePath = [[NSString alloc] initWithCString:argv[1]];
 	}
-	
 
     // we have to run the test in a secondary thread because we need to run a
     // runloop, which blocks
