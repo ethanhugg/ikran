0e25970a86002c0c834966d8e4417082b3123667
Ethan Hugg <ehugg@cisco.com>
2011-11-07 19:19:49 -0800

    Added test app for ROAP.

--- SConstruct
+++ SConstruct
@@ -284,7 +284,8 @@ SCRIPT_FILES = [
 
 # Unit tests and testapp builds are all static.
 SCRIPT_FILES += [ 
-  'tests/testapp_softphone/SConstruct'
+  'tests/testapp_softphone/SConstruct',
+  'tests/roap/SConstruct'
 ]
 
 if noaddon != 'yes':
---
+++ tests/roap/roap.php
@@ -0,0 +1,35 @@
+<?php
+
+$roap = $_POST['roap'];
+
+function sendToRoapProxy($message)
+{
+  $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+  $success = socket_connect($socket, "localhost", "1234");
+
+  socket_send($socket, $message . "\n", strlen($message) + 1, 0);
+
+  socket_close($socket);
+}
+
+if ($roap == Null)
+{
+?>
+
+{
+  "messageType":"OK",
+  "callerSessionId":"1234567890",
+  "calleeSessionId":"abcdefghij",
+  "seq":1
+}
+
+<?php
+}
+else
+{
+  sendToRoapProxy($roap);
+  echo("\nSuccess\n\n" . $roap);
+}
+?>
+
